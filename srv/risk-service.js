e4b2e32ed97ff391f30fcbac7f4a16241fddae04
StevenGong <423881+steven-g@users.noreply.github.com>
2022-11-30 01:35:33 +0000

    3.add.external.service.bugfix2

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -15,11 +15,13 @@ this.after("READ", Risks, (data) => {
   const risks = Array.isArray(data) ? data : [data];
 
   risks.forEach((risk) => {
-    if (risk.impact >= 100000) {
-      risk.criticality = 1;
-    } else {
-      risk.criticality = 2;
-    }
+    if (risk) {
+      if (risk.impact >= 100000) {
+        risk.criticality = 1;
+      } else {
+        risk.criticality = 2;
+      }
+  }
   });
 });
 
